322b4f38e41acb3b1d10040f9c86a1ca6feb5d85
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-04 09:27:34 +0000

    conversion

---
+++ basic/datatypecon.js
@@ -0,0 +1,18 @@
+let score="33abc";
+console.log(typeof score);
+let convo = Number(score)
+console.log(typeof convo);
+console.log(convo);
+//"33" = 33
+//"absc" = Nan
+
+let bb =1
+let cc=Boolean(bb)
+//console.log(cc);
+//1 = true 
+//0 = false
+// "" = false
+// "anything" = true
+ let some=5552
+ let st= String(some)
+ console.log(st)
