ace9ab2fd78366b6c35c63f5b8b9265e37068119
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-05 09:43:52 +0000

    strings

---
+++ basic/strings.js
@@ -0,0 +1,10 @@
+const name="Ashmit"
+let cg =9.1
+//console.log(`Hello my name is ${name} and my cgpa is ${cg}`)
+//console.log(name[4])
+const gg= new String("Ashmit")
+console.log(gg.length)
+console.log(gg.charAt(3))
+console.log(gg.slice(-4,-2))
+console.log(gg.replace('A',"s"))
+console.log(gg.includes('mit'))
