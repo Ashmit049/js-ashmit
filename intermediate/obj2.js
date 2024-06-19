4ad242244591cc6f1aa069f9ad1bd59e8507b903
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-19 18:12:23 +0000

    ob2

---
+++ intermediate/obj2.js
@@ -0,0 +1,20 @@
+const tin={}
+tin.name="Ash"
+tin.id="aa"
+tin.dob=22
+console.log(tin);
+
+
+
+const j={
+    fullname:{
+        usrname:{
+            firstname:"a"
+        }
+    }
+}
+console.log(j.fullname.usrname.firstname);
+const o={1:"a",2:"b"}
+const p={3:"c",5:"g"}
+const ob=Object.assign(o,p)
+console.log(ob);
