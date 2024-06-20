f6c9400cd7f45113a0463d4e794ce4f25b53407a
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-20 18:15:58 +0000

    morefunc

---
+++ intermediate/function2.js
@@ -0,0 +1,31 @@
+//function c(...a)
+{
+   // return a
+}
+//console.log(c(200,400,600));
+
+
+
+const abc={
+    name:"Ash",
+    price:400
+}
+
+function ex(any){
+    console.log(`this is ${any.name} and price is ${any.price}`)
+
+}
+ex(abc)
+
+
+
+
+
+
+
+const c=[2,3,4,5]
+function m(a)
+{
+    return a[1]
+}
+console.log(m(c))
