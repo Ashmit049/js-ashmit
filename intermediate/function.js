c0f45ea411f6bc534fe3707bc51f344ce2f19cd8
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-20 09:57:07 +0000

    function

---
+++ intermediate/function.js
@@ -0,0 +1,34 @@
+function my()
+{
+    console.log("Nitish")
+    
+}
+my()
+
+
+function hell(a,b)
+{
+    console.log(a+b)
+
+}
+hell(4,7)
+
+
+function ni(c,d)
+{
+    let s=c+d
+    return s
+}
+console.log(ni(6,5))
+
+
+
+
+
+
+function c(username="ashmit")
+{
+    return `${username} is logged in `
+
+}
+console.log(c());
