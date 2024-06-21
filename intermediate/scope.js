58e35b1dc2d384a9c903d5ed87a59fa8af929b94
Ashmit049 <149051046+Ashmit049@users.noreply.github.com>
2024-06-21 08:54:43 +0000

    scope

---
+++ intermediate/scope.js
@@ -0,0 +1,52 @@
+function one()
+{
+    const user1="ash"
+    function two()
+    {
+        const web1="yt"
+    console.log(user1+web1)
+    
+    }
+    //console.log(web)
+
+}
+one()
+
+
+
+if(true)
+    {
+        const user="Ash"
+        if(user=="Ash")
+            {
+                const web="yt"
+                console.log(user + web);
+
+            }
+            console.log(user)
+
+    }
+   // console.log(user)
+
+
+
+
+   //scope problem 
+
+   console.log(one())
+   function one(){
+    return 5
+   }
+   
+   console.log(a(5))
+   const a=function(num){
+    return 5+num
+   }
+   
+
+
+
+
+
+
+
